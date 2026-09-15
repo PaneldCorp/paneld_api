30ff1f2d0f7aa5ce24bd64f91963a0d2a355ec8b
0x5448594f <derive.theo@proton.me>
2026-09-16 01:32:27 +0200

    Fix: Update main file to handle new things

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use actix_web::{App, HttpServer};
+use actix_web::{
+    web::Data,
+    App,
+    HttpServer
+};
 
 use crate::{
-    config::database::Database,
+    config::{
+        database::Database,
+        services::Services
+    },
+
     utils::log::{Level, log}
 };
 
 mod config;
+mod models;
+mod services;
+mod repositories;
 mod utils;
+
+pub struct AppState
+{
+    pub services: Services
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let database = Database::init().await;
+    let services = Services::new(database.clone().pool);
+    let app_state = Data::new(services);
 
     log(Level::Info, format!("API serveur running on http://{}.", database.get_binding()).as_str());
 
-    HttpServer::new(||
+    HttpServer::new(move ||
         {
             App::new()
+                .app_data(app_state.clone())
         }
     )
     .bind(database.get_binding())?
