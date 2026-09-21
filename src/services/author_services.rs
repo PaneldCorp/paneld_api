a4c4885e9da56cd4e16d1ac993429a97497f8e16
0x5448594f <derive.theo@proton.me>
2026-09-21 10:38:35 +0200

    Feature: Add services to authors

--- src/services/author_services.rs
+++ src/services/author_services.rs
@@ -1,5 +1,7 @@
+use uuid::Uuid;
+
 use crate::{
-    models::author::Author,
+    models::author::{NewAuthor, Author},
     repositories::author_repository::AuthorRepo,
     config::error::Error
 };
@@ -23,6 +25,39 @@ impl AuthorServices
         match authors
         {
             Ok(authors) => Ok(authors),
+            Err(err) => Err(Error::from_sqlx_error(err))
+        }
+    }
+
+    pub async fn get_author_by_id(&self, id: Uuid) -> Result<Vec<Author>, Error>
+    {
+        let author = self.repo.get_author_by_id(id).await;
+
+        match author
+        {
+            Ok(author) => Ok(author),
+            Err(err) => Err(Error::from_sqlx_error(err))
+        }
+    }
+
+    pub async fn get_author_by_name(&self, name: String) -> Result<Vec<Author>, Error>
+    {
+        let author = self.repo.get_author_by_name(name).await;
+
+        match author
+        {
+            Ok(author) => Ok(author),
+            Err(err) => Err(Error::from_sqlx_error(err))
+        }
+    }
+
+    pub async fn new_author(&self, new_author: NewAuthor) -> Result<Author, Error>
+    {
+        let new_author = self.repo.new_author(new_author).await;
+
+        match new_author 
+        {
+            Ok(author) => Ok(author),
             Err(err) => Err(Error::from_sqlx_error(err))
         }
     }
