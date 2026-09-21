cf7788b65cb6ed55a899b066be78a600da08207b
0x5448594f <derive.theo@proton.me>
2026-09-21 10:35:46 +0200

    Fix: Add NewAuthor + updated field

--- src/models/author.rs
+++ src/models/author.rs
@@ -1,7 +1,8 @@
 use uuid::Uuid;
 use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
 
-#[derive(sqlx::FromRow, Debug)]
+#[derive(sqlx::FromRow, Debug, Serialize)]
 pub struct Author
 {
     pub id: Uuid,
@@ -9,5 +10,42 @@ pub struct Author
     pub name: String,
     pub birthday: NaiveDate,
     pub website: Option<String>,
-    pub description: Option<String>
+    pub description: Option<String>,
+    pub pub_date: NaiveDate,
+    pub update_date: NaiveDate
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewAuthor
+{
+    pub japanese_name: String,
+    pub name: String,
+    pub birthday: NaiveDate,
+    pub website: Option<String>,
+    pub description: Option<String>,
+}
+
+impl Author
+{
+    pub fn from_new_author(new_author: NewAuthor, uuid: Uuid, now: NaiveDate) -> Self
+    {
+        let NewAuthor {
+            japanese_name,
+            name,
+            birthday,
+            website,
+            description
+        } = new_author;
+
+        Author {
+            id: uuid,
+            japanese_name,
+            name,
+            birthday,
+            website,
+            description,
+            pub_date: now,
+            update_date: now
+        }
+    }
 }
