a32ae665286dfe79d2daa4279bf310a3bc4e837d
0x5448594f <derive.theo@proton.me>
2026-09-21 10:26:41 +0200

    Fix: Update sql author file

--- sql/author.sql
+++ sql/author.sql
@@ -3,6 +3,9 @@ Create table authors
     id uuid primary key unique not null,
     japanese_name varchar(255) not null unique,
     name varchar(255) not null unique,
+    birthday date not null,
     website varchar(255) null,
-    description text null
+    description text null,
+    pub_date date not null,
+    update_date date not null
 );
